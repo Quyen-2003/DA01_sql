f4ebf73bf4c9678682fa19853419aa2186bda713
Quyen-2003 <quyenphan.forwork@gmail.com>
2025-04-11 09:02:56 +0700

    Update Day_6.sql

--- Day_6.sql
+++ Day_6.sql
@@ -10,12 +10,13 @@ SELECT
 LOWER(first_name) AS TEN_KH,
 LOWER(last_name) AS HO_KH
 FROM customer
-WHERE length(first_name)<10
-OR ength(last_name)<10
+WHERE length(first_name)>10
+OR ength(last_name)>10
 -- 2) LEFT(), RIGHT()
 SELECT
-  LEFT(first_name,3)  -- hiển thị 3 kỹ tự đầu tiên của first_name
-  RIGHT(first_name,3)  -- hiển thị 3 kỹ tự cuối cùng của first_name
+  LEFT(first_name,3)  -- hiển thị 3 ký tự đầu tiên của first_name
+  RIGHT(first_name,3)  -- hiển thị 3 ký tự cuối cùng của first_name
+  RIGHT(LEFT(first_name,3),2) -- lấy 2 ký tự ở giữa. VD: MARY => _AR_  
 FROM customer 
 /*challenge: Trích xuất 5 ký tự cuối cùng của địa chỉ email.
 Làm thế nào để chỉ trích xuất dấu "." ở địa chỉ email*/
@@ -26,7 +27,8 @@ FROM customer
 SELECT customer_id,
 first_name ,
 last_name,
-first_name|| last_name AS full_name -- || dùng để nối chuỗi
+first_name|| ' ' ||last_name AS full_name -- || dùng để nối chuỗi hoặc dùng CONCAT
+CONCAT(first_name,' ', last_name) AS full_name
 FROM customer
 /*Giả sử bạn chỉ có địa chỉ email và họ của khách hàng.
 Bạn cần trích xuất tên từ địa chỉ email và nối nó với họ. 
@@ -42,6 +44,7 @@ FROM customer
 SELECT
 email,
 POSITION ('@' IN email) -- tìm vị trí của @ trong email
+LEFT(email, position('@' in email) - 1) -- tách phần thông tin họ & tên trong trường email
 FROM customer 
 -- 6) SUBSTRING()
 -- lấy ra ký tự từ 2 đến 4 của first_name trong bảng customer
@@ -50,11 +53,21 @@ SUBSTRING(first_name FROM 2 FOR 3)
 FROM customer
 -- lấy thông tin họ KH từ email
  SELECT email,
- SUBSTRING(email FROM POSITION ( '.' IN email) +1 FOR POSITION ( '@' IN email)-POSITION ( . IN email)-1) as HO_KH
+ SUBSTRING(email FROM POSITION ( '.' IN email) +1 FOR POSITION ( '@' IN email) - POSITION ( '.' IN email)-1) as HO_KH
 FROM customer 
+--challenge: trích xuất tên từ email và nối với họ. Kết quả phải ở dạng 'Họ, tên'
+select email, 
+substring(email from 1 for position('.'in email)-1) as first_name,
+substring(email from 1 for position('.'in email)-1) || ' , ' || last_name as ho_ten 
+FROM CUSTOMER
 -- 7) EXTRACT() dùng để trích xuất thông tin Năm, tháng, ngày, giờ.. của 1 date/datetime kết quả trả ra dưới dạng number
 SELECT * FROM rental
 WHERE EXTRACT( 'year' FROM rental_date) =2020
+-- năm 2020 có bao nhiêu đơn hàng cho thuê trong mỗi tháng
+select extract(month from rental_date), count(*) from rental
+where extract(year from rental_date)=2020
+group by extract(month from rental_date)
+/*challenge 
 -- 8) TO_CHAR () lấy thông tin datetime theo định dạng momg muốn(forrmat) 
 SELECT TO_CHAR(payment_date, 'year') 
 FROM payment
